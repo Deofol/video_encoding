f9778d4a92984399d1c04c8a966eb337ca2092d1
Deofol <deofol@protonmail.com>
2023-08-17 11:02:22 +0200

    new file:   video_encoding.sh

---
+++ video_encoding.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+input_dir="/path/to/input/directory"
+output_dir="/path/to/output/directory"
+codec="libx264" # Change this to the desired codec
+crf="23"        # Constant Rate Factor (adjust as needed)
+
+# Create the output directory if it doesn't exist
+mkdir -p "$output_dir"
+
+# Find all video files in the input directory and its subdirectories
+find "$input_dir" -type f -name "*.mp4" -o -name "*.mkv" -o -name "*.avi" | while read -r input_file; do
+    # Generate the output filename and directory structure
+    relative_path="${input_file#$input_dir}"
+    output_file="$output_dir${relative_path%.*}.mp4"
+
+    echo "Re-encoding: $input_file"
+
+    # Re-encode the video using ffmpeg
+    ffmpeg -i "$input_file" -c:v $codec -crf $crf "$output_file"
+done
+
+echo "Re-encoding complete!"
